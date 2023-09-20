e9efd9238ac7abab7b519b7ad461269f8bb4cb51
Tom Paz <tompazz16@gmail.com>
2023-09-21 01:59:27 +0300

    Added dockerfiles and containerizing the microservices to be able to be deployed as containers

--- dbribbit/dbribbit.py
+++ dbribbit/dbribbit.py
@@ -1,12 +1,21 @@
 import requests
 import pymongo
 import time
+import logging
+import os 
+
+# Set the logging level
+logging.basicConfig(level=logging.DEBUG)
+logging.getLogger("urllib3").setLevel(logging.WARNING)
+logging.getLogger("requests").setLevel(logging.WARNING)
 
 # URL of the PondPulse microservice
-pondpulse_url = "http://localhost:5000/microservices"
+pondpulse_url = "http://pondpulse:5000/microservices"
+#db_user = os.getenv('YOUR_ENV_VARIABLE_NAME')// to implement when deploying to k8s
+#db_pass = os.getenv('YOUR_ENV_VARIABLE_NAME')// to implement when deploying to k8s
 
 # Initialize a connection to MongoDB
-mongo_client = pymongo.MongoClient("mongodb://localhost:27017/")  # Replace with your MongoDB connection string
+mongo_client = pymongo.MongoClient("mongodb://root:example@mongo:27017/")  # Replace with your MongoDB connection string
 db = mongo_client["faulty_versions_db"]  # Replace with your database name
 collection = db["faulty_versions"]
 
@@ -20,12 +29,15 @@ def poll_and_persist_faulty_versions():
         # Check the state of each microservice
         for microservice, data in microservices_data.items():
             if data['state'] in ['insecure', 'slow']:
+                logging.info(f"found a faulty microservice {microservice}")
                 # Persist the faulty version to MongoDB
                 faulty_version = {
                     "microservice": microservice,
                     "version": data['version'],
+                    "state": data['state'],
                     "timestamp": int(time.time())
                 }
+                logging.info(f"writing to DB - {faulty_version}")
                 collection.insert_one(faulty_version)
     
     except requests.exceptions.RequestException as e:
@@ -34,4 +46,4 @@ def poll_and_persist_faulty_versions():
 if __name__ == '__main__':
     while True:
         poll_and_persist_faulty_versions()
-        time.sleep(1800)  # Poll every 30 minutes (adjust the interval as needed)
+        time.sleep(30)  # Poll every 30 minutes (adjust the interval as needed)
--- flytrap/flytrap.py
+++ flytrap/flytrap.py
@@ -1,9 +1,16 @@
 import requests
 import time
 import random
+import logging
+import os 
+
+# Set the logging level
+logging.basicConfig(level=logging.DEBUG)
+logging.getLogger("urllib3").setLevel(logging.WARNING)
+logging.getLogger("requests").setLevel(logging.WARNING)
 
 # URL of the PondPulse microservice
-pondpulse_url = "http://127.0.0.1:5000/microservices"
+pondpulse_url = "http://pondpulse:5000/microservices"
 
 # Function to check the health of microservices
 def check_microservices_health():
@@ -17,26 +24,25 @@ def check_microservices_health():
             if data['state'] == 'healthy':
                 # Simulate detecting a bug
                 if should_detect_bug():
-                    print(f"Detected bug in {microservice}. Modifying state to 'insecure'.")
-                    data['state'] = random.choice(['insecure', 'slow'])
+                    new_state = random.choice(['insecure', 'slow'])
+                    logging.debug(f"Detected bug in {microservice}. Modifying state to {new_state}.")
+                    data['state'] = new_state
                 else:
-                    print(f"No issues found in {microservice}.")
+                    logging.debug(f"No issues found in {microservice}.")
 
         # Send the updated data back to PondPulse
-        print(microservices_data)
-        print(type(microservices_data))
         response = requests.post(pondpulse_url + '/update', json=microservices_data)
         response.raise_for_status()
-        print("Microservices data updated successfully.")
+        logging.info("Microservices data updated successfully.")
 
     except requests.exceptions.RequestException as e:
-        print(f"Error connecting to PondPulse: {str(e)}")
+        logging.error(f"Error connecting to PondPulse: {str(e)}")
 
 # Function to simulate bug detection with a low (random) frequency
 def should_detect_bug():
-    return random.random() < 0.5  # Adjust the frequency as needed
+    return random.random() < 0.1  # Adjust the frequency as needed
 
 if __name__ == '__main__':
     while True:
         check_microservices_health()
-        time.sleep(30)  # Check every 5 minutes (adjust the interval as needed)
+        time.sleep(60)  # Check every 1 minutes (adjust the interval as needed)
